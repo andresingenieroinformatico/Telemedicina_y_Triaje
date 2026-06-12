45c16f440f3ec871236d2cad04ab8725752961f1
VictorGMZ4 <vhugogomez04@gmail.com>
2026-06-12 09:38:34 -0500

    Delete microservices/historial_medico directory

--- microservices/historial_medico/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth_routes     import auth_bp
-from .usuario_routes  import usuario_bp
-from .paciente_routes import paciente_bp
-from .medico_routes   import medico_bp
-from .historial_routes import historial_bp
-from .receta_routes   import receta_bp
-from .cita_routes     import cita_bp
