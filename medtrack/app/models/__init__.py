586a9e2a98b87ab3512097494ef342bb4a291f61
VictorGMZ4 <vhugogomez04@gmail.com>
2026-06-12 09:33:53 -0500

    Delete medtrack directory

--- medtrack/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .usuario  import Usuario
-from .paciente import Paciente
-from .medico   import Medico
-from .historial import HistorialMedico
-from .receta   import Receta
-from .cita     import Cita
